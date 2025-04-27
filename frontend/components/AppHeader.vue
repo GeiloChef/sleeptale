57b6d1316274001e041e3e77f39582941e28c730
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2025-04-27 13:43:56 +0200

    Add simple app header

---
+++ frontend/components/AppHeader.vue
@@ -0,0 +1,64 @@
+<template>
+  <Menubar class="flex justify-center items-center menubar">
+    <template #start>
+      {{ $t('sleeptale') }}
+    </template>
+  </Menubar>
+
+</template>
+
+<script setup lang="ts">
+
+const items = ref([
+  {
+    label: 'Home',
+    icon: 'pi pi-home'
+  },
+  {
+    label: 'Features',
+    icon: 'pi pi-star'
+  },
+  {
+    label: 'Projects',
+    icon: 'pi pi-search',
+    items: [
+      {
+        label: 'Components',
+        icon: 'pi pi-bolt'
+      },
+      {
+        label: 'Blocks',
+        icon: 'pi pi-server'
+      },
+      {
+        label: 'UI Kit',
+        icon: 'pi pi-pencil'
+      },
+      {
+        label: 'Templates',
+        icon: 'pi pi-palette',
+        items: [
+          {
+            label: 'Apollo',
+            icon: 'pi pi-palette'
+          },
+          {
+            label: 'Ultima',
+            icon: 'pi pi-palette'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    label: 'Contact',
+    icon: 'pi pi-envelope'
+  }
+]);
+</script>
+
+<style lang="scss" scoped>
+.menubar {
+  font-size: 1.2rem;
+}
+</style>
