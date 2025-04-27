8cadc13c2e06c8da5992ac84a6ee2ab1b0b1bcd8
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2025-04-27 13:44:03 +0200

    Add simple default layout

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="app-container h-screen flex flex-col">
+    <header class="header">
+      <AppHeader />
+    </header>
+
+    <main class="content py-16 px-8 flex-grow overflow-y-auto">
+      <slot />
+    </main>
+  </div>
+</template>
