d1ce31df3e052a4c60508cdf4c8438fe59184de0
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2025-05-03 11:56:40 +0200

    Add default story to for mockups

--- frontend/utils/Story.Utils.ts
+++ frontend/utils/Story.Utils.ts
@@ -1,11 +1,46 @@
-import type {StoryDto} from "@/types/Story.types";
+import type {Section, StoryDto} from "@/types/Story.types";
 
 export const createDefaultStory = (): StoryDto => {
   return {
-    createdAt: "",
-    sections: [],
-    title: "",
-    scheduledAt: null,
+    createdAt: '1746223200000',
+    sections: createDefaultSections(),
+    title: "Das Abenteuer der kleinen Eule Ella",
+    scheduledAt: '1746136800000',
     id: 0
   }
+}
+
+export const createDefaultSections = (): Section[] => {
+  return [
+    {
+      id: Math.random(),
+      text: 'This is my second section',
+      order: 1,
+      storyId: 0
+    },
+    {
+      id: Math.random(),
+      text: 'This is my third section',
+      order: 2,
+      storyId: 0
+    },
+    {
+      id: Math.random(),
+      text: 'This is my first section',
+      order: 0,
+      storyId: 0
+    },
+    {
+      id: Math.random(),
+      text: 'This is my fourth section',
+      order: 3,
+      storyId: 0
+    },
+    {
+      id: Math.random(),
+      text: 'This is my fifth section',
+      order: 4,
+      storyId: 0
+    },
+  ]
 }
