27cb9042638f6ec7fb7de7130c2b9693fbbd6dbd
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2025-05-03 11:57:37 +0200

    Add date-based story page that fetches the story for the passed date string

---
+++ frontend/pages/story/[date].vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    hallo test
+  </div>
+</template>
+
+
+<script setup lang="ts">
+
+import {onMounted} from "vue";
+import {useRoute} from "nuxt/app";
+import {MomentFormat} from "@/types/Core.Types";
+
+const route = useRoute();
+
+const storyStore = useStoryStore();
+
+onMounted(async (): Promise<void> => {
+  console.log()
+  if (route.params.date) {
+    await storyStore.fetchStoryByDate(moment(route.params.date, MomentFormat.Date));
+  }
+})
+</script>
