7bf10720c71241efe3fe5e270b66a2f00e5bbca9
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2025-05-23 23:41:15 +0200

    Add font size to application store

--- frontend/stores/Application.Store.ts
+++ frontend/stores/Application.Store.ts
@@ -1,9 +1,31 @@
 import {defineStore} from "pinia";
 
-export const useStoryStore = defineStore('storyStore', () => {
+export const useApplicationStore = defineStore('applicationStore', () => {
   const isMenuOpen = ref(false);
 
+  const fontSize = ref(24);
+
+  const increaseFontSize = (): void => {
+    if (fontSize.value < 32) {
+      fontSize.value = fontSize.value + 1
+    }
+  }
+
+  const decreaseFontSize = (): void => {
+    if (fontSize.value > 16) {
+      fontSize.value = fontSize.value - 1
+    }
+  }
+
   return {
-    isMenuOpen
+    isMenuOpen,
+    fontSize,
+    increaseFontSize,
+    decreaseFontSize
+  }
+}, {
+  persist: {
+    storage: piniaPluginPersistedstate.sessionStorage(),
+    pick: ['fontSize'],
   }
 });
