4146d2426754f12db70599ddc9512302ee8a4edb
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2025-05-23 23:40:32 +0200

    Add custom icons for increasing and decreasing font size

---
+++ frontend/components/icons/DecreaseFontSizeIcon.vue
@@ -0,0 +1,6 @@
+<template>
+  <span class="fa-layers fa-fw" style="font-size: 1em">
+      <Icon icon="font" style="color: white; font-size: .9em;"></Icon>
+      <Icon icon="minus" style="color: white; transform: translate(0.8em, -0.8em); font-size: 0.6em;"></Icon>
+    </span>
+</template>
