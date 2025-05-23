6763c806cceb319daa65e4b3f498956faa401d0b
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2025-05-23 23:40:59 +0200

    Add story settings bar and include buttons to change font size

---
+++ frontend/components/StorySettingsBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-row justify-between">
+    <div class="flex flex-row gap-3">
+      <DecreaseFontSizeIcon @click="applicationStore.decreaseFontSize" />
+      <IncreaseFontSizeIcon @click="applicationStore.increaseFontSize" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import IncreaseFontSizeIcon from "@/components/icons/IncreaseFontSizeIcon.vue";
+import DecreaseFontSizeIcon from "@/components/icons/DecreaseFontSizeIcon.vue";
+import { useApplicationStore } from "@/stores/Application.Store";
+
+const applicationStore = useApplicationStore();
+</script>
