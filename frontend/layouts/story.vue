f019e9071b0d7927eabdfac017d3fde32d20188a
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2025-05-27 01:24:36 +0200

    Initial work on story page - push for saving data from corruption

---
+++ frontend/layouts/story.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app-container flex flex-col h-screen">
+    <header class="header">
+      <AppHeader />
+    </header>
+
+    <aside>
+      <AppMenu />
+    </aside>
+
+    <main class="flex flex-grow overflow-y-auto h-full w-screen">
+      <slot />
+    </main>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
