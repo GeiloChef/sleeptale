eaee36cf5ca2558cadb0a725c0767ddb5355bff7
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2025-05-27 22:44:13 +0200

    Add text to speech output for story

--- frontend/pages/story/[date].vue
+++ frontend/pages/story/[date].vue
@@ -5,11 +5,15 @@
       <Image
         v-if="selectedStory.imageUrl"
         :src="imageUrl(selectedStory.imageUrl)" />
-      <StorySettingsBar class="pt-6 px-4" />
+      <StorySettingsBar
+          class="pt-6 px-4"
+          @getTextToSpeechForSection="onGetTextToSpeechForSection" />
+      <audio ref="AudioPlayer_Ref" style="display: none" />
     </div>
 
     <div>
       <Carousel
+          ref="StorySectionCarousel_Ref"
           :value="selectedStory.sections"
           :numVisible="1"
           :numScroll="1"
@@ -33,6 +37,7 @@ import {onMounted} from "vue";
 import {MomentFormat} from "@/types/Core.Types";
 import {useRoute} from "nuxt/app";
 import {imageUrl} from "@/utils/Image.Utils";
+import {audioUrl} from "@/utils/Audio.Utils";
 
 definePageMeta({
   layout: 'story',
@@ -45,6 +50,18 @@ const { selectedStory } = storeToRefs(storyStore);
 
 const applicationStore = useApplicationStore();
 
+const StorySectionCarousel_Ref = ref();
+const AudioPlayer_Ref = ref<HTMLAudioElement>();
+
+const onGetTextToSpeechForSection = async (): Promise<void> => {
+  console.log(StorySectionCarousel_Ref.value.value[StorySectionCarousel_Ref.value.d_page])
+  const currentSection = StorySectionCarousel_Ref.value.value[StorySectionCarousel_Ref.value.d_page]
+  const textToSpeechUrl = await storyStore.fetchTextToSpeechForStory(selectedStory.value.id, currentSection.id);
+  if (AudioPlayer_Ref.value) {
+    AudioPlayer_Ref.value.src = audioUrl(textToSpeechUrl);
+    await AudioPlayer_Ref.value.play();
+  }
+}
 
 onMounted(async (): Promise<void> => {
   if (route.params.date) {
