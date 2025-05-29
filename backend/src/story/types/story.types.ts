43fe6fa3272a8dbe992727c899276ee248e18636
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2025-05-29 19:54:35 +0200

    Update genre object

--- backend/src/story/types/story.types.ts
+++ backend/src/story/types/story.types.ts
@@ -24,132 +24,132 @@ export type StoryFromDatabase = Story & {
 export const STORY_GENRES: StoryGenre[] = [
   {
     key: 'bedtime',
-    label: 'Bedtime',
+    label: 'genre.bedtime',
     ageGroups: [StoryAgeGroup.Kids],
     promptModifier:
       'Die Geschichte soll beruhigend sein, mit einem friedlichen Ende abschließen und ideal als gute Nacht Geschichte geeignet sein.',
   },
   {
     key: 'adventure',
-    label: 'Adventure',
+    label: 'genre.adventure',
     ageGroups: [StoryAgeGroup.Kids, StoryAgeGroup.Teens, StoryAgeGroup.Adults],
     promptModifier: 'Die Geschichte soll abenteuerlich sein und ',
   },
   {
     key: 'fantasy',
-    label: 'Fantasy',
+    label: 'genre.fantasy',
     ageGroups: [StoryAgeGroup.Kids, StoryAgeGroup.Teens, StoryAgeGroup.Adults],
     promptModifier:
       'Die Geschichte soll Elemente von Magie, Fabelwesen oder einer fantastischen Welt enthalten.',
   },
   {
     key: 'sci-fi',
-    label: 'Science Fiction',
+    label: 'genre.science-fiction',
     ageGroups: [StoryAgeGroup.Teens, StoryAgeGroup.Adults],
     promptModifier:
       'Die Geschichte soll in der Zukunft oder in einem technologischen Umfeld spielen.',
   },
   {
     key: 'romance',
-    label: 'Romance',
+    label: 'genre.romance',
     ageGroups: [StoryAgeGroup.Teens, StoryAgeGroup.Adults],
     promptModifier:
       'Es soll sich eine romantische Handlung zwischen zwei Personen entwickeln.',
   },
   {
     key: 'comedy',
-    label: 'Comedy',
+    label: 'genre.comedy',
     ageGroups: [StoryAgeGroup.Kids, StoryAgeGroup.Teens, StoryAgeGroup.Adults],
     promptModifier:
       'Die Geschichte soll humorvoll und unterhaltsam sein und den Leser zum Lachen bringe.',
   },
   {
     key: 'educational',
-    label: 'Educational',
+    label: 'genre.educational',
     ageGroups: [StoryAgeGroup.Kids, StoryAgeGroup.Teens],
     promptModifier:
       'Die Geschichte soll lehrreich sein und dem Leser auf spielerische Weise Wissen vermitteln.',
   },
   {
     key: 'mystery',
-    label: 'Mystery',
+    label: 'genre.mystery',
     ageGroups: [StoryAgeGroup.Teens, StoryAgeGroup.Adults],
     promptModifier:
       'Baue eine rätselhafte oder unerwartete Wendung in die Geschichte ein, die den Leser eine angenehme Aufregung spüren lässt.',
   },
   {
     key: 'horror',
-    label: 'Horror',
+    label: 'genre.horror',
     ageGroups: [StoryAgeGroup.Teens, StoryAgeGroup.Adults],
     promptModifier:
       'Die Geschichte soll düster, mysteriös und leicht beängstigend sein.',
   },
   {
     key: 'thriller',
-    label: 'Thriller',
+    label: 'genre.thriller',
     ageGroups: [StoryAgeGroup.Adults],
     promptModifier:
       'Die Geschichte soll spannend, nervenaufreibend und voller unerwarteter Wendungen sein. Sie darf gerne sehr schaurig sein!',
   },
   {
     key: 'drama',
-    label: 'Drama',
+    label: 'genre.drama',
     ageGroups: [StoryAgeGroup.Teens, StoryAgeGroup.Adults],
     promptModifier:
       'Die Geschichte soll emotionale Tiefe haben und zwischenmenschliche Konflikte thematisieren.',
   },
   {
     key: 'historical',
-    label: 'Historical',
+    label: 'genre.historical',
     ageGroups: [StoryAgeGroup.Teens, StoryAgeGroup.Adults],
     promptModifier:
       'Die Geschichte soll in einem historischen Kontext spielen und authentische Ereignisse oder Figuren einbeziehen.',
   },
   {
     key: 'slice-of-life',
-    label: 'Slice of Life',
+    label: 'genre.slice-of-life',
     ageGroups: [StoryAgeGroup.Kids, StoryAgeGroup.Teens, StoryAgeGroup.Adults],
     promptModifier:
       'Die Geschichte soll alltägliche Situationen und zwischenmenschliche Beziehungen realistisch darstellen.',
   },
   {
     key: 'mythology',
-    label: 'Mythology',
+    label: 'genre.mythology',
     ageGroups: [StoryAgeGroup.Kids, StoryAgeGroup.Teens, StoryAgeGroup.Adults],
     promptModifier:
       'Die Geschichte soll Elemente aus bekannten oder fiktiven Mythen, Göttern und Sagen enthalten.',
   },
   {
     key: 'folktale',
-    label: 'Folktale',
+    label: 'genre.folktale',
     ageGroups: [StoryAgeGroup.Kids, StoryAgeGroup.Teens, StoryAgeGroup.Adults],
     promptModifier:
       'Die Geschichte soll den Stil klassischer Volksmärchen haben und eine klare moralische Botschaft enthalten.',
   },
   {
     key: 'supernatural',
-    label: 'Supernatural',
+    label: 'genre.supernatural',
     ageGroups: [StoryAgeGroup.Teens, StoryAgeGroup.Adults],
     promptModifier:
       'Die Geschichte soll übernatürliche Elemente wie Geister, Flüche oder Unerklärliches beinhalten.',
   },
   {
     key: 'psychological',
-    label: 'Psychological',
+    label: 'genre.psychological',
     ageGroups: [StoryAgeGroup.Adults],
     promptModifier:
       'Die Geschichte soll psychologisch tiefgründig sein und innere Konflikte oder mentale Prozesse thematisieren.',
   },
   {
     key: 'dystopian',
-    label: 'Dystopian',
+    label: 'genre.dystopian',
     ageGroups: [StoryAgeGroup.Teens, StoryAgeGroup.Adults],
     promptModifier:
       'Die Geschichte soll in einer dystopischen Zukunft spielen und gesellschaftliche Missstände überzeichnen.',
   },
   {
     key: 'feel-good',
-    label: 'Feel-Good',
+    label: 'genre.feel-good',
     ageGroups: [StoryAgeGroup.Kids, StoryAgeGroup.Teens, StoryAgeGroup.Adults],
     promptModifier:
       'Die Geschichte soll in einer dystopischen Zukunft spielen und gesellschaftliche Missstände überzeichnen.',
