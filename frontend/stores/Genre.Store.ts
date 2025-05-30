e3c0cce405dc74d04de2b1f01d01cd24a48391e0
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2025-05-30 19:25:10 +0200

    Add genre store

---
+++ frontend/stores/Genre.Store.ts
@@ -0,0 +1,22 @@
+import { defineStore } from 'pinia'
+import { useGenres } from "@/.nuxt/imports";
+import type {Genre} from "@/types/Genre.types";
+export const useGenreStore = defineStore('genreStore', () => {
+  const { getAllGenres } = useGenres();
+
+  const allAvailableGenres = ref<Genre[]>([]);
+
+  const fetchAllAvailableGenres = async (): Promise<void> => {
+    allAvailableGenres.value = await getAllGenres();
+  }
+
+  return {
+    allAvailableGenres,
+    fetchAllAvailableGenres
+  }
+}, {
+  persist: {
+    storage: piniaPluginPersistedstate.sessionStorage(),
+    pick: ['allAvailableGenres'],
+  }
+});
