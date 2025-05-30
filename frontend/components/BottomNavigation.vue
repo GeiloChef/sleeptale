0928e20149d714223645218de66b5a1e64f7ed90
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2025-05-30 23:31:35 +0200

    Add bottom navigation

---
+++ frontend/components/BottomNavigation.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="w-full border-t border-t-white flex justify-around items-center h-20">
+    <div
+      v-for="item in navigationItems"
+      :key="item.label"
+      class="flex flex-col gap-2 w-1/4"
+      @click="item.callback">
+      <Icon
+          size="lg"
+          :icon="item.icon" />
+      <span class="text-xs text-center italic">
+        {{ item.label }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import {MomentFormat} from "@/types/Core.Types";
+
+const { t } = useI18n();
+
+const navigationItems = [
+  {
+    label: t('home'),
+    icon: 'home',
+    callback: () => {navigateByRouteName('index')}
+  },
+  {
+    label: t('daily-story'),
+    icon: 'calendar-days',
+    callback: () => {navigateToStoryPage(moment().format(MomentFormat.DateUrl))}
+  },
+  {
+    label: t('profile'),
+    icon: 'user',
+  },
+]
+</script>
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -4,12 +4,11 @@
       <AppHeader />
     </header>
 
-    <aside>
-      <AppMenu />
-    </aside>
-
-    <main class="flex h-full w-screen py-8">
-      <slot />
+    <main class="flex flex-col flex-grow w-screen min-h-0">
+      <div class="flex-grow overflow-y-auto px-4 py-8 min-h-0">
+        <slot />
+      </div>
+      <BottomNavigation class="flex-shrink-0" />
     </main>
   </div>
 </template>
