156342f3921330a6155c68e2e5ed8e114982b649
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2025-06-03 23:09:10 +0200

    Add user profile / settings page

---
+++ frontend/pages/user/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col gap-8">
+    <span class="text-2xl text-center">
+      {{ $t('user-settings') }}
+    </span>
+    <div class="flex flex-col gap-4">
+      <Divider />
+      <AgeGroupPreferenceSelection />
+      <Divider />
+      <div class="flex flex-row justify-between items-center">
+        <span class="text-lg italic">
+          {{ $t('language') }}
+        </span>
+      <LanguageSelect class="w-3/5" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import LanguageSelect from "@/components/LanguageSelect.vue";
+</script>
