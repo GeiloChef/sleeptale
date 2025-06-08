f11f50bfe93352cb44ed7e3e38a75152c6765839
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2025-06-09 00:09:55 +0200

    Improve user store to work with the user class

--- frontend/stores/User.Store.ts
+++ frontend/stores/User.Store.ts
@@ -2,8 +2,29 @@ import {defineStore} from "pinia";
 import {User} from "@/types/classes/User.Class";
 import {getAvailableLanguages} from "@/.nuxt/imports";
 
+
 export const useUserStore = defineStore('userStore', () => {
   const user = ref(new User(getAvailableLanguages()[0]));
+
+  const hydrateUser = (data: any): User => {
+    return Object.assign(new User(data.language), data);
+  }
+
+  if (process.client && sessionStorage.getItem('userStore')) {
+    try {
+      const parsed = JSON.parse(sessionStorage.getItem('userStore')!);
+      if (parsed.user) {
+        user.value = hydrateUser(parsed.user);
+      }
+    } catch (e) {
+      console.error('Failed to hydrate user from sessionStorage:', e);
+    }
+  }
+
+  if (!user.value) {
+    user.value = new User(getAvailableLanguages()[0]);
+  }
+
 
   return {
     user
