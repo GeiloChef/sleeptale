d90ce763977cf13d1293e0385a595ef2ec4aae98
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2025-07-03 00:31:40 +0200

    Add spanish as language

--- backend/src/common/utils/language.utils.ts
+++ backend/src/common/utils/language.utils.ts
@@ -1,4 +1,4 @@
-import {FALLBACK_LANGUAGE} from "../../story/types/story.types";
+import { FALLBACK_LANGUAGE } from '../../story/types/story.types';
 
 export function getLanguageFromHeader(
   headerValue?: string,
@@ -8,7 +8,7 @@ export function getLanguageFromHeader(
 
   const languages = headerValue.split(',').map((l) => l.trim().toLowerCase());
 
-  const supported = ['de', 'en'];
+  const supported = ['de', 'en', 'es'];
   const firstValid = languages.find((lang) => supported.includes(lang));
 
   return firstValid ?? fallback;
